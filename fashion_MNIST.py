2be10c760accd0168f7c440533b2dd23803e29bc
Idden <idden.tsai@gmail.com>
2025-12-27 21:08:37 -0600

    attempt at improving accuracy

--- fashion_MNIST.py
+++ fashion_MNIST.py
@@ -106,14 +106,26 @@ class CNN(nn.Module):
         super().__init__()
 
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
+
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(32 * 14 * 14, 128)
+
+        self.drop = nn.Dropout(0.25)
+
+        self.fc1 = nn.Linear(128 * 7 * 7, 128)
         self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = self.pool(x)
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = F.relu(self.bn3(self.conv3(x)))
         x = torch.flatten(x, start_dim=1)
+        x = self.drop(x)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
 
@@ -127,7 +139,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 # training loop
-num_epochs = 3
+num_epochs = 25
 
 for epoch in range(num_epochs):
     model.train()
@@ -137,11 +149,11 @@ for epoch in range(num_epochs):
         images = images.to(device)
         labels = labels.to(device)
 
-        optimizer.zero_grad()        # reset gradients
-        outputs = model(images)      # forward
+        optimizer.zero_grad()
+        outputs = model(images)
         loss = criterion(outputs, labels)
-        loss.backward()              # backward
-        optimizer.step()             # update weights
+        loss.backward()
+        optimizer.step()
 
         running_loss += loss.item()
 
